a8fc3d3faaebf6b630727e163637e42a218e33f4
PARESH PATIL <pareshpatilprof@gmail.com>
2025-12-19 23:58:07 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -7,12 +7,8 @@ const userRoutes = require('./routes/userRoutes');
 const courseRoutes = require('./routes/courseRoutes');
 
 const app = express();
-
-// Middleware
 app.use(cors());
 app.use(express.json());
-
-// Database connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -20,16 +16,12 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log('MongoDB connected successfully'))
 .catch((err) => console.log('MongoDB connection error:', err));
 
-// Routes
 app.use('/api/users', userRoutes);
 app.use('/api/courses', courseRoutes);
 
-// Home route
 app.get('/', (req, res) => {
   res.send('Course Management API is running');
 });
-
-// Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Something went wrong!' });
