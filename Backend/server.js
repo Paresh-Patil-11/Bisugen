b9f58211a02a7c8184ecc3f60f3d55b36999b1ea
PARESH PATIL <pareshpatilprof@gmail.com>
2025-12-20 01:18:18 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
 
 const userRoutes = require('./routes/userRoutes');
 const courseRoutes = require('./routes/courseRoutes');
@@ -13,6 +13,9 @@ app.use(cors({
 }));
 
 app.use(express.json());
+
+console.log('Current mongo url is:', process.env.MONGO_URI);  
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected successfully'))
   .catch((err) => console.log('MongoDB connection error:', err));
