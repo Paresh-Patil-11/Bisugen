cf7124e4bd6017d3d11712f24506a3833d28729c
PARESH PATIL <pareshpatilprof@gmail.com>
2025-12-20 12:42:17 +0530

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
-import { API_URL } from './api';
+import { API_URL } from '../api.js';
 
 function Login({ setUser }) {
   const navigate = useNavigate();
