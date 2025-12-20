5892ace943e9258e3278b983967bf0e9f58af998
PARESH PATIL <pareshpatilprof@gmail.com>
2025-12-20 12:05:31 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -20,7 +20,10 @@ app.use(express.json());
 
 console.log('Current mongo url is:', process.env.MONGO_URI);  
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log('MongoDB connected successfully'))
   .catch((err) => console.log('MongoDB connection error:', err));
 
