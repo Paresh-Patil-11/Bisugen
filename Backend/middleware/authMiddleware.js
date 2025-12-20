419619a973806967520b19ff90ae9cf0908aba39
PARESH PATIL <pareshpatilprof@gmail.com>
2025-12-20 12:04:51 +0530

    Update authMiddleware.js

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -8,18 +8,16 @@ const protect = async (req, res, next) => {
     try {
       token = req.headers.authorization.split(' ')[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      
       req.user = await User.findById(decoded.id).select('-password');
-      
-      next();
+      return next(); // Add return here
     } catch (error) {
       console.log(error);
-      res.status(401).json({ message: 'Not authorized, token failed' });
+      return res.status(401).json({ message: 'Not authorized, token failed' });
     }
   }
 
   if (!token) {
-    res.status(401).json({ message: 'Not authorized, no token' });
+    return res.status(401).json({ message: 'Not authorized, no token' }); // Add return
   }
 };
 
