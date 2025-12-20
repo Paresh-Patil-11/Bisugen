3f53c5f8f115201300eaf21ea887da8cc9a62f2b
PARESH PATIL <pareshpatilprof@gmail.com>
2025-12-20 12:42:40 +0530

    Update CreateCourse.jsx

--- Frontend/src/components/CreateCourse.jsx
+++ Frontend/src/components/CreateCourse.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { API_URL } from './api';
+import { API_URL } from '../api.js';
 
 function CreateCourse({ user }) {
   const navigate = useNavigate();
